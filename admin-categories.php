5d60e6baad2b382b553277990ef30d6ec22e4e24
Sergio Alves <sergio.alves@dce.ufpb.br>
2018-12-20 10:46:27 -0300

    Categorias Vs Produtos

--- admin-categories.php
+++ admin-categories.php
@@ -8,6 +8,8 @@ use Hcode\Model\User;
 
 use Hcode\Model\Category;
 
+use Hcode\Model\Product;
+
 $app -> get("/admin/categories", function(){
 
 	User::verifyLogin();
@@ -83,16 +85,57 @@ $app -> post("/admin/categories/:idcategory", function($idcategory){
 
 	header("Location: /admin/categories");
 	exit;
-
 });
 
-$app -> get("/categories/:idrecovery", function($idrecovery){
+$app -> get("/admin/categories/:idcategory/products", function($idcategory){
+
+	User::verifyLogin();
+	
 	$category = new Category();
 
-	$category->get((int)$idrecovery );
+	$category->get((int)$idcategory );
 
-	$page = new Page();
-	$page -> setTpl("category", ["category" => $category -> getValues(), "products" => [] ]);
+	$page = new PageAdmin();
+
+	$page -> setTpl("categories-products", ["category" => $category -> getValues(), "productsRelated" => $category->getProducts(), "productsNotRelated" => $category->getProducts(false) ]);
+});
+
+
+$app -> get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct ){
+
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory );
+
+	$product = new Product();
+
+	$product ->get((int)$idproduct);
+
+	$category -> addProduct($product);
+
+	header("Location: /admin/categories/".$idcategory."/products");
+	exit;
+});
+
+
+$app -> get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct ){
+
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory );
+
+	$product = new Product();
+
+	$product ->get((int)$idproduct);
+
+	$category -> removeProduct($product);
+
+	header("Location: /admin/categories/".$idcategory."/products");
+	exit;
 });
 
  ?>
